8dc645b0b15e254a6faa0d7cd4cc8bc74e0c44b6
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-27 18:31:27 -0400

    complete

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,10 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+
+export default function CoordinatesButton(props) {
+    return (
+        <button onClick={e=>props.onReceiveCoordinates([e.clientX, e.clientY])}>Coordinates</button>
+
+    )
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,18 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+
+export default function DelayedButton(props) {
+
+    const clickHandler = (e) => {
+        e.persist()
+         window.setTimeout(() => {
+            props.onDelayedClick(e)
+            props.delay})
+    }
+
+    return (
+        <button onClick={clickHandler}>Hi</button>
+
+    )
+}
